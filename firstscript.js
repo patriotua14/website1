062e579b6cb9058c6ef834d37528237403d5060f
patriotua14 <pirat-32@ukr.net>
2016-02-27 21:45:42 +0000

    readyto

--- firstscript.js
+++ firstscript.js
@@ -1,3 +1,23 @@
 // JavaScript File
 $('div.container').css("width","100%");
 $('div.container').css("padding","0");
+//www.google material disign.com
+//wow.js
+//jquery scrollto
+//yahoo ytl
+$('a[href^="#"]').click(function () {
+    var elementClick = $(this).attr("href");
+    var destination = $(elementClick).offset().top;
+    $("body, html").animate( {
+        scrollTop: destination}, 800);
+        return false;
+    })
+    
+$("#navbar").removeClass("default");
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 10) {
+        $("#navbar").addClass("default").fadeIn("fast");    
+    } else {
+        $("#navbar").removeClass("default").fadeIn("fast");
+    };
+});
