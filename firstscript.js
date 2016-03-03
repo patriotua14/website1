57ad7578e8c7efc178d0037b077b1543fe0e63cb
patriotua14 <pirat-32@ukr.net>
2016-03-03 21:47:21 +0000

    workfinish

--- firstscript.js
+++ firstscript.js
@@ -25,7 +25,7 @@ $(".house").on('click', function(e) {
    modal.css('z-index', '500');
    modal.css('border-radius', '50px');
    $("body").css('background-color', '#F5F5F5');
-   
+   $('.hard').toggleClass('hard1');
    modal.show(timeAnimate);
 }) ; 
 
@@ -73,7 +73,7 @@ $(".game").on('click', function(e) {
 
 $(".safe").on('click', function(e) {
    var modal = $('#modalDiv');
-   modal.css('top', (window.innerHeight - modal.height()) / 4);
+   modal.css('top', (window.innerHeight - modal.height()) / 2);
    modal.css('left', (window.innerWidth - modal.width()) / 2);
    modal.css('background-image', 'url(http://blackrockdigital.github.io/startbootstrap-freelancer/img/portfolio/safe.png)');
    modal.css('background-repeat', 'no-repeat');
@@ -107,15 +107,11 @@ $(".boat").on('click', function(e) {
 $('#modalDiv').on('click', function () {
    var modal = $('#modalDiv');
     modal.hide(timeAnimate);
-})
+});
 
-/*$('<p>In 1939, after the Polish September Campaign and the accompanying Soviet invasion, the Soviet occupiers permitted classes to continue. Until late 1939, the school worked in the pre-war Polish system. On October 18, however, the Polish rector, Professor Roman Longchamps de Bérier was dismissed, and was replaced by a prominent Ukrainian historian, Mykhailo Marchenko, grandfather of Ukrainian journalist and dissident Valeriy Marchenko. Marchenko was determined to transform the University of Lwow into the Ukrainian National University.[3] On January 8, 1940, the university was renamed Ivan Franko Lviv State University. Polish professors and administrative assistants were increasingly fired and replaced by Ukrainians or Russians, specializing in Marxism, Leninism, political economics, as well as Ukrainian and Soviet literature, history and geography. This was accompanied by the closing of departments seen as related with the religion, free-market economics, capitalism, or the West in general; this included Polish geography, literature, or history. Lectures were held in Ukrainian and Polish (as auxiliary). In the period 1939-1941 the Soviets also executed over a dozen members of the Polish faculty.</p>').css({ 
-    'color': 'black',
-    'font-size': '25px',
-}
-    ).appendTo('#modalDiv');
-    
-    $('#somanytext').on('click', function () {
-         var text1 = $('#somanytext');
-              text1.hide(timeAnimate);
-    })*/
+
+ $('.handle').on('click', function()
+           {
+               $('nav ul').toggleClass('showing');
+ });
+ 
